1eb437cd23da83095b142de2df3378fd66135a34
eitan.suez <eitan.suez@45af1514-b755-11dd-bba1-afcbec1a1e1f>
2008-11-20 23:34:45 +0000

    added ant build file

--- gturtle.sh
+++ gturtle.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-java -classpath /home/eitan/work/thirdparty/groovy/embeddable/groovy-all-1.6-beta-2.jar:lib/jsyntaxpane-0.9.3.jar:out/production/GTurtle gturtle.TurtleConsole
+java -classpath /home/eitan/work/thirdparty/groovy/embeddable/groovy-all-1.6-beta-2.jar:lib/jsyntaxpane-0.9.3.jar:out gturtle.TurtleConsole
+
+# alternative:
+#java -classpath dist/GTurtle.jar:lib/jsyntaxpane-0.9.3.jar:/home/eitan/work/thirdparty/groovy/embeddable/groovy-all-1.6-beta-2.jar gturtle.TurtleConsole
+# also can use "ant run"
+# make sure groovy_home is set
 
