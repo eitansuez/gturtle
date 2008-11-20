931767131e659e5cb852ed04dba44e1c32e47e93
eitan.suez <eitan.suez@45af1514-b755-11dd-bba1-afcbec1a1e1f>
2008-11-20 23:21:17 +0000

    minor .sh file fix

--- gturtle.sh
+++ gturtle.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -classpath /home/eitan/work/thirdparty/groovy/embeddable/groovy-all-1.6-beta-2.jar:lib/jsyntaxpane-0.9.3.jar:out/production/turtle_groovy gturtle.TurtleConsole
+java -classpath /home/eitan/work/thirdparty/groovy/embeddable/groovy-all-1.6-beta-2.jar:lib/jsyntaxpane-0.9.3.jar:out/production/GTurtle gturtle.TurtleConsole
 
