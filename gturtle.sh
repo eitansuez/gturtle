734ae49d5126f6adb4b8bcfde4e22a5f23c80d84
eitan.suez <eitan.suez@45af1514-b755-11dd-bba1-afcbec1a1e1f>
2008-11-20 23:38:03 +0000

    small tweak to sh file

--- gturtle.sh
+++ gturtle.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-java -classpath /home/eitan/work/thirdparty/groovy/embeddable/groovy-all-1.6-beta-2.jar:lib/jsyntaxpane-0.9.3.jar:out gturtle.TurtleConsole
+java -classpath $GROOVY_HOME/embeddable/groovy-all-1.6-beta-2.jar:lib/jsyntaxpane-0.9.3.jar:out gturtle.TurtleConsole
 
 # alternative:
 #java -classpath dist/GTurtle.jar:lib/jsyntaxpane-0.9.3.jar:/home/eitan/work/thirdparty/groovy/embeddable/groovy-all-1.6-beta-2.jar gturtle.TurtleConsole
