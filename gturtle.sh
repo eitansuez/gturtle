7617c063e6fafc0ccc55ec1600f3c8acb199487d
eitan.suez <eitan.suez@45af1514-b755-11dd-bba1-afcbec1a1e1f>
2008-11-25 20:06:05 +0000

    minor/ignore (readme comment)

--- gturtle.sh
+++ gturtle.sh
@@ -3,9 +3,11 @@
 # make sure groovy_home is set
 
 cwd=`dirname "$0"`
-java -classpath $GROOVY_HOME/embeddable/groovy-all-1.6-beta-2.jar:$cwd/lib/jsyntaxpane-0.9.3.jar:$cwd/out gturtle.TurtleConsole
+groovyalljar=groovy-all-1.5.7.jar
+
+java -classpath $GROOVY_HOME/embeddable/$groovyalljar:$cwd/lib/jsyntaxpane-0.9.3.jar:$cwd/out gturtle.TurtleConsole
 # alternative:
-#java -classpath $cwd/dist/GTurtle.jar:$cwd/lib/jsyntaxpane-0.9.3.jar:$GROOVY_HOME/embeddable/groovy-all-1.6-beta-2.jar gturtle.TurtleConsole
+#java -classpath $cwd/dist/GTurtle.jar:$cwd/lib/jsyntaxpane-0.9.3.jar:$GROOVY_HOME/embeddable/$groovyalljar gturtle.TurtleConsole
 
 # also can use "ant run"
 
